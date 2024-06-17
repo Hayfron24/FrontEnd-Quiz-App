8ab470792aaccb278000bd2cc34b3f65bea84031
Hayfron24 <isaachayfron24@gmail.com>
2024-06-17 13:36:07 +0000

    index page design complete

--- src/app/index-page/index-page.component.ts
+++ src/app/index-page/index-page.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NgFor, NgStyle } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-index-page',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule,NgFor,NgStyle],
   templateUrl: './index-page.component.html',
   styleUrl: './index-page.component.css'
 })
-export class IndexPageComponent {
+export class IndexPageComponent implements OnInit{
   data: any;
 
-  // constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) {}
 
-  // ngOnInit(): void {
-  //   this.dataService.getData().subscribe((response) => {
-  //     this.data = response;
-  //     console.log(response);
-  //   });
-  // }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((response) => {
+      this.data = response.quizzes;
+      console.log(this.data);
+    });
+  }
 }
